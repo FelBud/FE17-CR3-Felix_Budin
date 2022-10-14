aaafdb371cfc87ef930c8305e88585132f2dd57f
Felix Budin <felix_budin@gmx.net>
2022-10-14 10:49:26 +0200

    product details and order button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+const routes: Routes = [
+   {
+      path: "", component: HomeComponent
+   },
+  {
+     path: "product-list", component: ProductListComponent
+  },
+  {
+     path: 'product-list/products/:productId', component: ProductDetailsComponent
+  },
+  {
+     path: 'cart', component: CartComponent
+},
+{
+   path: "about", component: AboutComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductDetailsComponent,
+    CartComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
