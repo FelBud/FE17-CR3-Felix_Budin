3b1c5c6af2503944874b4cfab99e6c396158649a
Felix Budin <felix_budin@gmx.net>
2022-10-14 10:04:33 +0200

    created navbar and products

---
+++ src/product.ts
@@ -0,0 +1,45 @@
+export const products = [
+
+    {
+        name: "Pizza Inferno",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, porro.",
+        img: "assets/img/inferno.jpg",
+        price: 12.90
+        },
+        {   
+        name: "Pizza Capricciosa",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, porro.",
+        img: "assets/img/capricciosa.jpg",
+        price: 15.50
+        },
+        {   
+        name: "Pizza Margerita",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, porro.",
+        img: "assets/img/margerita.jpg",
+        price: 19.50
+        },
+        {
+        name: "Pizza Prosciutto",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, porro.",
+        img: "assets/img/prosciutto.jpg",
+        price: 12.90
+        },
+        {
+        name: "Penna al Gorgonzola",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, porro.",
+        img: "assets/img/penne.jpg",
+        price: 12.30
+        },
+        {
+        name: "Spaghetti Carbonara",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, porro.",
+        img: "assets/img/carbonara.jpg",
+        price: 15.70
+        },
+        {
+        name: "Tiramisu",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, porro.",
+        img: "assets/img/tiramisu.jpg",
+        price: 10.20
+        },
+     ];
